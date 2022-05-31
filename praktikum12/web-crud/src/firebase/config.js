887e06bcb28af7e545ac1e09f6e684cb4599e3e2
TasyaSakinah30 <tasya00661@gmail.com>
2022-05-31 21:03:07 +0700

    first commit

---
+++ praktikum12/web-crud/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmX0umkYGIxGD3jqSQeLCr-sgPOMJpUt8",
+    authDomain: "praktikum11-42d97.firebaseapp.com",
+    projectId: "praktikum11-42d97",
+    storageBucket: "praktikum11-42d97.appspot.com",
+    messagingSenderId: "691600500060",
+    appId: "1:691600500060:web:729d7b8b23b0a8d1d44d99",
+    measurementId: "G-5M1F4H070V"
+  };
+
+  export default firebaseConfig;
