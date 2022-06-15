2228fea10355251e4480f644914e32a79f4cfb3a
Muhammad-Ridho <ramadhanjenius1@gmail.com>
2022-06-15 07:03:45 +0700

    edit produk

--- project/web-project/src/components/Product.js
+++ project/web-project/src/components/Product.js
@@ -9,7 +9,7 @@ const Product = () => {
     return(
         <div class="container mt-3">
             <div class="row">
-                <div class="col">
+                <div class="col mt-1">
                     <div class="card">
                         <img src="https://cf.shopee.co.id/file/0c89ee68b866da3787df5a0ea8afd4c2" class="card-img-top" alt="..." />
                         <div class="card-body">
@@ -21,7 +21,7 @@ const Product = () => {
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col mt-1">
                     <div class="card">
                         <img src="https://cf.shopee.co.id/file/0c89ee68b866da3787df5a0ea8afd4c2" class="card-img-top" alt="..." />
                         <div class="card-body">
@@ -33,7 +33,7 @@ const Product = () => {
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col mt-1">
                     <div class="card">
                         <img src="https://cf.shopee.co.id/file/0c89ee68b866da3787df5a0ea8afd4c2" class="card-img-top" alt="..." />
                         <div class="card-body">
@@ -45,7 +45,7 @@ const Product = () => {
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col mt-1">
                     <div class="card">
                         <img src="https://cf.shopee.co.id/file/0c89ee68b866da3787df5a0ea8afd4c2" class="card-img-top" alt="..." />
                         <div class="card-body">
@@ -57,43 +57,7 @@ const Product = () => {
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="https://cf.shopee.co.id/file/0c89ee68b866da3787df5a0ea8afd4c2" class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title"><strong>Set Pisau Dapur 5 in 1 Premium</strong></h5>
-                            <p class="card-text">Set Pisau Dapur/ Pisau daging dan Multifungsi/ Gunting Dapur Pengasah Pisau.</p>
-                            
-                            <h4>Rp. 245.000</h4>
-                            <a href="#" class="btn btn-primary">Beli</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="https://cf.shopee.co.id/file/0c89ee68b866da3787df5a0ea8afd4c2" class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title"><strong>Set Pisau Dapur 5 in 1 Premium</strong></h5>
-                            <p class="card-text">Set Pisau Dapur/ Pisau daging dan Multifungsi/ Gunting Dapur Pengasah Pisau.</p>
-                            
-                            <h4>Rp. 245.000</h4>
-                            <a href="#" class="btn btn-primary">Beli</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="https://cf.shopee.co.id/file/0c89ee68b866da3787df5a0ea8afd4c2" class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title"><strong>Set Pisau Dapur 5 in 1 Premium</strong></h5>
-                            <p class="card-text">Set Pisau Dapur/ Pisau daging dan Multifungsi/ Gunting Dapur Pengasah Pisau.</p>
-                            
-                            <h4>Rp. 245.000</h4>
-                            <a href="#" class="btn btn-primary">Beli</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
+                <div class="col mt-1">
                     <div class="card">
                         <img src="https://cf.shopee.co.id/file/0c89ee68b866da3787df5a0ea8afd4c2" class="card-img-top" alt="..." />
                         <div class="card-body">
