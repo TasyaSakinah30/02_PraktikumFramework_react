5c6b3d0274e7a079aec76b1bc5ed47d50ac9427d
TasyaSakinah30 <tasya00661@gmail.com>
2022-06-17 08:22:51 +0700

    first

--- project/web-project/src/components/About.js
+++ project/web-project/src/components/About.js
@@ -10,10 +10,12 @@ const About = () => {
                         <p className="lead">
                             Toko Peralatan Dapur.</p>
                         <p className="lead">Kelas: TI-3D</p>
+                        <p className="lead">M. Ridho Ramadhan | Tasya Rachma Sakinah</p>
                     </div>
                     <div className="col-md-6 d-flex justify-content-center">
                     <img src="/assets/images/toko.jpg" alt="About Us" align="center" height="300px" width="500px"/>
                     </div>
+
                 </div>
             </div>
         </div>
